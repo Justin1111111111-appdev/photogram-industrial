b70b2665292e252d8e437c224375025faa8c45ad
Justin1111111111 <stuwrkr-jguzm21@mx.uillinois.edu>
2023-04-05 19:49:04 +0000

    Got started with project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :projects
+  
+authenticate :user, lambda { |u| u.admin? } do 
+  mount Sidekiq::web => "/sidekiq"
+end
+
+  devise_for :users
+  root to: "projects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
