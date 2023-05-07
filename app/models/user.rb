7af4befbc40e1939f348681f7bd75b76283f0cf6
Justin1111111111 <stuwrkr-jguzm21@mx.uillinois.edu>
2023-05-07 01:44:06 +0000

    did liked photos and posts links

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,10 @@ class User < ApplicationRecord
 
   has_many :comments, foreign_key: :author_id
 
+  has_many :likes, foreign_key: :fan_id
+
+  has_many :liked_photos, through: :likes, source: :photo
+
   has_many :discover, through: :leaders, source: :liked_photos
 
   has_many :feed, through: :leaders, source: :own_photos
@@ -41,10 +45,6 @@ class User < ApplicationRecord
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
 
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
-
-  has_many :liked_photos, through: :likes, source: :photo
-
-  has_many :likes, foreign_key: :fan_id
 
   has_many :own_photos, class_name: "Photo", foreign_key: :owner_id
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,16 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#liked", as: :liked_photos
+  get ":username/followers" => "photos#liked", as: :liked_photos
+  get ":username/following" => "photos#liked", as: :liked_photos
+
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
 
-    get "/:username" => "users#show"
+    get ":username" => "users#show", as: :user
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
